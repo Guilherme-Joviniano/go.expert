f1dedc0382d35b40fccc38a55eee3bdece3706bb
Guilherme-Joviniano <00drpixelss@gmail.com>
2023-12-18 11:58:04 -0300

    chore: CRUD methods with go-chi

--- apis/cmd/server/main.go
+++ apis/cmd/server/main.go
@@ -8,6 +8,8 @@ import (
 	"github.com/Guilherme-Joviniano/go.expert/apis/internal/entity"
 	database "github.com/Guilherme-Joviniano/go.expert/apis/internal/infra/database/sqlite"
 	"github.com/Guilherme-Joviniano/go.expert/apis/internal/infra/webserver/handlers"
+	"github.com/go-chi/chi/v5"
+	"github.com/go-chi/chi/v5/middleware"
 	"gorm.io/driver/sqlite"
 	"gorm.io/gorm"
 )
@@ -30,7 +32,14 @@ func main() {
 
 	productService := database.NewProductRepository(db)
 	productHandler := handlers.NewProductHandler(productService)
+
+	router := chi.NewRouter()
+	router.Use(middleware.Logger)
+	router.Post("/products", productHandler.CreateProduct)
+	router.Get("/products/{id}", productHandler.GetProduct)
+	router.Put("/products/{id}", productHandler.UpdateProduct)
+	router.Delete("/products/{id}", productHandler.DeleteProduct)
 	
-	http.HandleFunc("/products", productHandler.CreateProduct)
-	http.ListenAndServe(":8000", nil)
+
+	http.ListenAndServe(":8000", router)
 }
--- apis/internal/infra/webserver/handlers/product-handlers.go
+++ apis/internal/infra/webserver/handlers/product-handlers.go
@@ -1,12 +1,16 @@
 package handlers
 
 import (
+	"encoding/json"
 	"net/http"
+	"strings"
 
 	"github.com/Guilherme-Joviniano/go.expert/apis/internal/dto"
 	"github.com/Guilherme-Joviniano/go.expert/apis/internal/entity"
 	database "github.com/Guilherme-Joviniano/go.expert/apis/internal/infra/database/schemas"
+	entityPKG "github.com/Guilherme-Joviniano/go.expert/apis/pkg/entity"
 	"github.com/Guilherme-Joviniano/go.expert/apis/pkg/util"
+	"github.com/go-chi/chi/v5"
 )
 
 type ProductHandler struct {
@@ -20,11 +24,6 @@ func NewProductHandler(service database.ProductInterface) *ProductHandler {
 }
 
 func (h *ProductHandler) CreateProduct(w http.ResponseWriter, req *http.Request) {
-	if req.Method != http.MethodPost {
-		w.WriteHeader(http.StatusMethodNotAllowed)
-		return
-	}
-
 	product, err := util.RequestToTypeAdapter[dto.CreateProductInput](req.Body)
 
 	if err != nil {
@@ -49,4 +48,87 @@ func (h *ProductHandler) CreateProduct(w http.ResponseWriter, req *http.Request)
 	}
 
 	w.WriteHeader(http.StatusCreated)
+}
+
+func (h *ProductHandler) GetProduct(w http.ResponseWriter, req *http.Request) {
+	id := chi.URLParam(req, "id")
+
+	if strings.EqualFold(id, "") {
+		w.WriteHeader(http.StatusBadRequest)
+		return
+	}
+
+	product, err := h.ProductService.GetById(id)
+
+	if err != nil {
+		w.WriteHeader(http.StatusNotFound)
+		return
+	}
+
+	w.WriteHeader(http.StatusOK)
+	json.NewEncoder(w).Encode(product)
+}
+
+func (h *ProductHandler) UpdateProduct(w http.ResponseWriter, req *http.Request) {
+	id := chi.URLParam(req, "id")
+
+	if strings.EqualFold(id, "") {
+		w.WriteHeader(http.StatusBadRequest)
+		return
+	}
+
+	var product entity.Product
+
+	productId, err := entityPKG.ParseID(id)
+
+	if err != nil {
+		w.WriteHeader(http.StatusBadRequest)
+		return
+	}
+
+	productBody, err := util.RequestToTypeAdapter[dto.UpdateProductInput](req.Body)
+
+	if err != nil {
+		w.WriteHeader(http.StatusBadRequest)
+		return
+	}
+
+	product.Id = productId
+	product.Name = productBody.Name
+	product.Price = productBody.Price
+
+	updatedProduct, err := h.ProductService.Update(&product)
+
+	if err != nil {
+		w.WriteHeader(http.StatusInternalServerError)
+		return
+	}
+
+	json.NewEncoder(w).Encode(updatedProduct)
+}
+
+func (h *ProductHandler) DeleteProduct(w http.ResponseWriter, req *http.Request) {
+	id := chi.URLParam(req, "id")
+
+	if strings.EqualFold(id, "") {
+		w.WriteHeader(http.StatusBadRequest)
+		return
+	}
+
+	_, err := entityPKG.ParseID(id)
+
+	if err != nil {
+		w.WriteHeader(http.StatusBadRequest)
+		return
+	}
+
+	// TODO: check different errors types to switch between http responses
+	err = h.ProductService.Delete(id)
+
+	if err != nil {
+		w.WriteHeader(http.StatusInternalServerError)
+		return
+	}
+
+	w.WriteHeader(http.StatusOK)
 }
